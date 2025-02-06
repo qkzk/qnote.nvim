8533b8225b93179929d2031234d3da9be4d6e511
qkzk <qu3nt1n@gmail.com>
2025-02-06 22:11:35 +0100

    fetch

--- lua/qnote/init.lua
+++ lua/qnote/init.lua
@@ -5,4 +5,18 @@ function M.hello()
 	print("Hello, world!")
 end
 
+function M.fetch_todos()
+	local url = "https://qkzk.ddns.net:4000/api/get_todos"
+	local response = vim.fn.systemlist("curl -s " .. url)
+
+	if vim.v.shell_error ~= 0 then
+		print("Erreur lors de la récupération des todos.")
+		return
+	end
+
+	-- Ouvre un buffer et affiche la réponse brute
+	vim.api.nvim_command("new")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, response)
+end
+
 return M
