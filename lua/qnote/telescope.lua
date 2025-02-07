471f901410cd8f051dc63243fc142676190bb002
qkzk <qu3nt1n@gmail.com>
2025-02-07 22:02:17 +0100

    telescope hides preview

--- lua/qnote/telescope.lua
+++ lua/qnote/telescope.lua
@@ -18,26 +18,42 @@ function M.pick_todo(todos)
 						value = todo,
 						display = string.format("[%d] %s (%s)", todo.id, todo.title, kind),
 						ordinal = todo.title,
-						preview = function(_, entry, status)
-							local preview_bufnr = status.preview_bufnr
-							local lines = {}
+						previewer = previewers.new_buffer_previewer({
+							define_preview = function(self, entry)
+								local content = entry.value.content
+								local text = ""
 
-							if entry.value.content.Text then
-								table.insert(lines, entry.value.content.Text)
-							elseif entry.value.content.Checkboxes then
-								table.insert(lines, "**TODO:**")
-								for _, item in ipairs(entry.value.content.Checkboxes.todo) do
-									table.insert(lines, "- [ ] " .. item)
+								if content.Text then
+									text = content.Text
+								elseif content.Checkboxes then
+									local todo_items = table.concat(content.Checkboxes.todo, "\n- [ ] ")
+									local done_items = table.concat(content.Checkboxes.done, "\n- [x] ")
+									text = string.format("# TODO\n- [ ] %s\n\n# DONE\n- [x] %s", todo_items, done_items)
 								end
-								table.insert(lines, "")
-								table.insert(lines, "**DONE:**")
-								for _, item in ipairs(entry.value.content.Checkboxes.done) do
-									table.insert(lines, "- [x] " .. item)
-								end
-							end
 
-							vim.api.nvim_buf_set_lines(preview_bufnr, 0, -1, false, lines)
-						end,
+								vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(text, "\n"))
+							end,
+						}),
+						-- preview = function(_, entry, status)
+						-- 	local preview_bufnr = status.preview_bufnr
+						-- 	local lines = {}
+						--
+						-- 	if entry.value.content.Text then
+						-- 		table.insert(lines, entry.value.content.Text)
+						-- 	elseif entry.value.content.Checkboxes then
+						-- 		table.insert(lines, "**TODO:**")
+						-- 		for _, item in ipairs(entry.value.content.Checkboxes.todo) do
+						-- 			table.insert(lines, "- [ ] " .. item)
+						-- 		end
+						-- 		table.insert(lines, "")
+						-- 		table.insert(lines, "**DONE:**")
+						-- 		for _, item in ipairs(entry.value.content.Checkboxes.done) do
+						-- 			table.insert(lines, "- [x] " .. item)
+						-- 		end
+						-- 	end
+						--
+						-- 	vim.api.nvim_buf_set_lines(preview_bufnr, 0, -1, false, lines)
+						-- end,
 					}
 				end,
 			}),
