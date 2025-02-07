d3008a83479eafab29a07dfd6f30a2268daadf47
qkzk <qu3nt1n@gmail.com>
2025-02-07 12:46:54 +0100

    add login

--- lua/qnote/init.lua
+++ lua/qnote/init.lua
@@ -83,6 +83,7 @@ end
 local telescope_qnote = require("qnote.telescope")
 
 function M.show_todos()
+	M.login()
 	local todos_json = M.fetch_todos()
 	if not todos_json then
 		print("Erreur : impossible de récupérer les todos")
