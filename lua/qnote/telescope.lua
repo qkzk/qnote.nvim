845bfd2d8c5349b4237a74b8d1776fb8a16421d3
qkzk <qu3nt1n@gmail.com>
2025-02-10 22:50:09 +0100

    document telescope module

--- lua/qnote/telescope.lua
+++ lua/qnote/telescope.lua
@@ -7,33 +7,40 @@ local action_state = require("telescope.actions.state")
 
 local M = {}
 
+---Fill the preview buffer of telescope with todo content
+---@param entry table
+---@param bufnr number
+local function fill_preview(entry, bufnr)
+	-- Fill preview
+	local lines = {}
+	table.insert(lines, "# " .. entry.value.title)
+	table.insert(lines, "")
+	if entry.value.content.Text then
+		vim.list_extend(lines, vim.split(entry.value.content.Text, "\n", { plain = true }))
+	elseif entry.value.content.Checkboxes then
+		table.insert(lines, "**TODO:**")
+		for _, item in ipairs(entry.value.content.Checkboxes.todo) do
+			vim.list_extend(lines, vim.split("- [ ] " .. item, "\n", { plain = true }))
+		end
+		table.insert(lines, "")
+		table.insert(lines, "**DONE:**")
+		for _, item in ipairs(entry.value.content.Checkboxes.done) do
+			vim.list_extend(lines, vim.split("- [x] " .. item, "\n", { plain = true }))
+		end
+	end
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+end
+
+---Creates a telescope entry for this todo
+---@param todo table
+---@return table
 local function make_entry(todo)
-	-- Affichage dans le picker
 	local kind = todo.content.Text and "Text" or "Checkbox"
 	return {
 		value = todo,
 		display = string.format("[%d] %s (%s)", todo.id, todo.title, kind),
 		ordinal = todo.title,
-		preview_command = function(entry, bufnr)
-			-- Remplit la preview
-			local lines = {}
-			table.insert(lines, "# " .. entry.value.title)
-			table.insert(lines, "")
-			if entry.value.content.Text then
-				vim.list_extend(lines, vim.split(entry.value.content.Text, "\n", { plain = true }))
-			elseif entry.value.content.Checkboxes then
-				table.insert(lines, "**TODO:**")
-				for _, item in ipairs(entry.value.content.Checkboxes.todo) do
-					vim.list_extend(lines, vim.split("- [ ] " .. item, "\n", { plain = true }))
-				end
-				table.insert(lines, "")
-				table.insert(lines, "**DONE:**")
-				for _, item in ipairs(entry.value.content.Checkboxes.done) do
-					vim.list_extend(lines, vim.split("- [x] " .. item, "\n", { plain = true }))
-				end
-			end
-			vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-		end,
+		preview_command = fill_preview,
 	}
 end
 local function make_finder(todos)
@@ -43,33 +50,46 @@ local function make_finder(todos)
 	}
 end
 
--- function M.pick_todo(todos)
--- 	pickers
--- 		.new({}, {
--- 			prompt_title = "Todos",
--- 			finder = finders.new_table(make_finder(todos)),
--- 			sorter = conf.generic_sorter({}),
--- 			previewer = previewers.new_buffer_previewer({
--- 				define_preview = function(self, entry, status)
--- 					if entry and entry.preview_command then
--- 						entry.preview_command(entry, self.state.bufnr)
--- 					end
--- 				end,
--- 			}),
--- 			attach_mappings = function(prompt_bufnr, map)
--- 				actions.select_default:replace(function()
--- 					actions.close(prompt_bufnr)
--- 					local selection = action_state.get_selected_entry()
--- 					if selection then
--- 						require("qnote").open_todo(selection.value)
--- 					end
--- 				end)
--- 				return true
--- 			end,
--- 		})
--- 		:find()
--- end
+---Creates telescope keybinds
+---<Return> opens a todo
+---<C-a> toggle archived state of the todo
+---<C-d> deletes the todo from server
+---@param prompt_bufnr number
+---@param map table
+---@return boolean
+local function create_mappings(prompt_bufnr, map)
+	-- <Return> opens a todo
+	actions.select_default:replace(function()
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		if selection then
+			require("qnote").open_todo(selection.value)
+		end
+	end)
 
+	-- <C-a> toggle archived state of the todo
+	map("i", "<C-a>", function()
+		local selection = action_state.get_selected_entry()
+		if selection then
+			actions.close(prompt_bufnr)
+			require("qnote").send_qnote_request("PATCH", "toggle_archived", selection.value.id)
+		end
+	end)
+
+	-- <C-d> deletes the todo from server
+	map("i", "<C-d>", function()
+		local selection = action_state.get_selected_entry()
+		if selection then
+			actions.close(prompt_bufnr)
+			require("qnote").send_qnote_request("DELETE", "delete_todo", selection.value.id)
+		end
+	end)
+
+	return true
+end
+
+---Action executed when a todo is picked.
+---@param todos table
 function M.pick_todo(todos)
 	pickers
 		.new({}, {
@@ -83,36 +103,7 @@ function M.pick_todo(todos)
 					end
 				end,
 			}),
-			attach_mappings = function(prompt_bufnr, map)
-				-- Ouvrir une note sur entrée
-				actions.select_default:replace(function()
-					actions.close(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					if selection then
-						require("qnote").open_todo(selection.value)
-					end
-				end)
-
-				-- Archiver avec <C-a>
-				map("i", "<C-a>", function()
-					local selection = action_state.get_selected_entry()
-					if selection then
-						actions.close(prompt_bufnr)
-						require("qnote").send_qnote_request("PATCH", "toggle_archived", selection.value.id)
-					end
-				end)
-
-				-- Supprimer avec <C-d>
-				map("i", "<C-d>", function()
-					local selection = action_state.get_selected_entry()
-					if selection then
-						actions.close(prompt_bufnr)
-						require("qnote").send_qnote_request("DELETE", "delete_todo", selection.value.id)
-					end
-				end)
-
-				return true
-			end,
+			attach_mappings = create_mappings,
 		})
 		:find()
 end
